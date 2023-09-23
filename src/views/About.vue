989f40bbc87354ec568c05c9bd6a8ef59cfd0851
ryoha000 <the7uya@gmail.com>
2023-09-24 04:54:58 +0900

    add hash router

---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Here is About Page</h1>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Here is Home Page</h1>
+</template>
